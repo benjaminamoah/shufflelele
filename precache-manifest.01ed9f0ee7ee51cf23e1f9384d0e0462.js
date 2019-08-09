37ea4750a572d94ae793627ee34fe9e7a341d0b0
Benjamin Amoah <33426921+benjaminamoah@users.noreply.github.com>
2019-08-09 11:23:36 -0400

    Add files via upload

---
+++ precache-manifest.01ed9f0ee7ee51cf23e1f9384d0e0462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4c8661f912ee0cc6ff712a2f264448",
+    "url": "/benjaminamoah/shufflelele.git/index.html"
+  },
+  {
+    "revision": "65216bf491b34ee00621",
+    "url": "/benjaminamoah/shufflelele.git/static/css/main.df253763.chunk.css"
+  },
+  {
+    "revision": "fb91d3aac997df3c8b95",
+    "url": "/benjaminamoah/shufflelele.git/static/js/2.627d0b4c.chunk.js"
+  },
+  {
+    "revision": "65216bf491b34ee00621",
+    "url": "/benjaminamoah/shufflelele.git/static/js/main.98e22615.chunk.js"
+  },
+  {
+    "revision": "226cae3a467002bd3d47",
+    "url": "/benjaminamoah/shufflelele.git/static/js/runtime~main.7da4b82e.js"
+  }
+]);
